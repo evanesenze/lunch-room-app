889a4e514317cf6602f392c33df53d86d307e4b8
Ivan <evanesenze@gmail.com>
2023-01-27 10:44:35 +0500

    fix

--- App.tsx
+++ App.tsx
@@ -4,14 +4,17 @@ import { View } from 'react-native';
 import Layout from './src/components/Layout.component';
 import { Provider } from 'react-redux';
 import { store } from './src/store';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App: React.FC = () => {
   return (
     <Provider store={store}>
-      <View style={{ flex: 1 }}>
-        <StatusBar style="auto" />
-        <Layout />
-      </View>
+      <NavigationContainer>
+        <View style={{ flex: 1 }}>
+          <StatusBar style="auto" />
+          <Layout />
+        </View>
+      </NavigationContainer>
     </Provider>
   );
 };
--- src/components/Layout.component.tsx
+++ src/components/Layout.component.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import Navigation from "./Navigation.component";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Home from "../screens/Home.screen";
-import Orders from "../screens/Orders.screen";
-import Cart from "../screens/Cart.screen";
-import Profile from "../screens/Profile.screen";
-import Auth from "../screens/Auth.screen";
+import React from 'react';
+import Navigation from './Navigation.component';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home.screen';
+import Orders from '../screens/Orders.screen';
+import Cart from '../screens/Cart.screen';
+import Profile from '../screens/Profile.screen';
+import Auth from '../screens/Auth.screen';
 
 export type AppParamsList = {
   Home: undefined;
@@ -22,18 +21,17 @@ const Tab = createBottomTabNavigator<AppParamsList>();
 
 const Layout: React.FC = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={{ headerShown: false, unmountOnBlur: true }}
-        tabBar={(props) => <Navigation {...(props as any)} />}
-      >
-        <Tab.Screen name="Auth" component={Auth} />
-        <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Orders" component={Orders} />
-        <Tab.Screen name="Cart" component={Cart} />
-        <Tab.Screen name="Profile" component={Profile} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    // <View>
+    //   <Text>Test</Text>
+    // </View>
+    // <Auth />
+    <Tab.Navigator screenOptions={{ headerShown: false, unmountOnBlur: true }} tabBar={(props) => <Navigation {...(props as any)} />}>
+      <Tab.Screen name="Auth" component={Auth} />
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Orders" component={Orders} />
+      <Tab.Screen name="Cart" component={Cart} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
   );
 };
 
